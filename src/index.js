a54e4c272c227223131265e898d8cca5385df302
misakisu <[che.v.jq@gmail.com]>
2020-09-14 19:45:26 +0900

    チュートリアル(-stateリフトアップ)

--- src/index.js
+++ src/index.js
@@ -23,48 +23,18 @@ function Square (props) {
 //
 /*どのマス目に何が入っているのかを管理しているのは Board です(suqares)*/
 class Board extends React.Component {
-  constructor(props) {
-  	super(props);
-  	this.state = {
-      squares: Array(9).fill(null),/*Squaresに指示送る(初期値設定)*/
-  	  xIsNext: true,
-  	};
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]){
-      return;
-    }
-
-    squares[i] = this.state.xIsNext ? 'x' : 'o';
-    this.setState({
-      squares: squares,/*現在の値を代入？これは何をしているのか。なぜ53行目はthis.state.squaresはだめ？*/
-      xIsNext: !this.state.xIsNext,/*xIsNext の値を反転させる*/
-    });
-  }
-
   renderSquare(i) {
     return(
       <Square
-        value={this.state.squares[i]}
-        onClick = {() => this.handleClick(i)}  /*BoardからSquare に現在の値i（'X'、'O' または null）を伝える*/
+        value={this.props.squares[i]}
+        onClick = {() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -87,20 +57,64 @@ class Board extends React.Component {
 
 //
 class Game extends React.Component {
+  /*Board にある state をップレベルの Game コンポーネントにリフトアップ*/
+  constructor(props){
+  	super(props);
+  	this.state = {
+      history: [{
+        squares: Array(9).fill(null),/*初期化*/
+      }],
+      xIsNext: true,
+  	};
+  }
+
+  handleClick(i) {
+  	const history = this.state.history;
+  	const current = history[history.length - 1]
+  	const squares = current.squares.slice();
+    if (calculateWinner(squares) || squares[i]){
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'x' : 'o';
+    this.setState({
+      history: history.concat([{
+        squares: squares,/*現在の値を代入？これは何をしているのか。なぜ53行目はthis.state.squaresはだめ？*/
+      }]),
+      xIsNext: !this.state.xIsNext,/*xIsNext の値を反転させる*/
+    });
+  }
+
+
+
   render() {
+    const history = this.state.history;
+    const current = history[history.length - 1];
+    const winner = calculateWinner(current.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board
+            squares={current.squares}
+            onClick={(i)=> this.handleClick(i)}/*GameからBoard、そしてSquare に現在の値i（'X'、'O' または null）を伝える*/
+           />
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
     );
   }
 }
+
 //
 
 function calculateWinner(squares) {
